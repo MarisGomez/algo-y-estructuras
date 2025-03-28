f9fae367201e358c1b97f022b2f8eeb586ec53d0
Marilin Gómez <gomezmarilin614@gmail.com>
2025-03-28 18:40:17 -0300

    Create Recordatorio.java

---
+++ Talleres/taller2/Recordatorio.java
@@ -0,0 +1,44 @@
+package aed;
+
+public class Recordatorio {
+    private String mensaje;
+    private Fecha fecha;
+    private Horario horario;
+
+    public Recordatorio(String mensaje, Fecha fecha, Horario horario) {
+        this.mensaje = mensaje;
+        this.fecha = new Fecha(fecha);
+        this.horario = horario;
+    }
+
+    public Horario horario() {
+        return horario;
+    }
+
+    public Fecha fecha() {
+        return fecha;
+    }
+
+    public String mensaje() {
+        return mensaje;
+    }
+
+    @Override
+    public String toString() {
+        return mensaje + " @ " + fecha + " " + horario;
+    }
+
+    @Override
+    public boolean equals(Object otro) {
+        boolean otroEsNull = (otro == null);
+        boolean claseDistinta = otro.getClass() != this.getClass();
+
+        if (otroEsNull || claseDistinta) {
+            return false;
+        }
+    Recordatorio otroRecordatorio = (Recordatorio) otro;
+    
+    return mensaje == otroRecordatorio.mensaje() && fecha == otroRecordatorio.fecha() && horario == otroRecordatorio.horario();
+    }
+
+}
