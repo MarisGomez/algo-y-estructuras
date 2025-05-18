f49ab7ea4ac546b9ec4bd1ceabface0f3ff3bd72
Marilin Gomez <gomezmarilin614@gmail.com>
2025-05-18 01:12:00 -0300

    update taller 4

---
+++ Talleres/taller4/src/main/java/aed/ABB.java
@@ -0,0 +1,155 @@
+package aed;
+
+import java.util.*;
+
+// Todos los tipos de datos "Comparables" tienen el método compareTo()
+// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
+public class ABB<T extends Comparable<T>> implements Conjunto<T> {
+    private Nodo raiz;
+    private int cardinal; //cantidad de elementos
+
+    private class Nodo {
+        T valor;
+        Nodo izq;
+        Nodo der;
+        Nodo padre;
+
+    public Nodo(T v) {
+        this.valor = v;
+        this.izq = null;
+        this.der = null;
+        this.padre = null;
+        }
+    }
+
+    public ABB() {
+        raiz = null;
+        cardinal = 0;
+    }
+
+    public int cardinal() {
+        return cardinal;
+    }
+
+    public T minimo(){
+        if(cardinal == 0){
+            return null;
+        }else {
+            Nodo actual = this.raiz;
+            while (actual.izq != null) {
+                actual = actual.izq;
+            }
+            return actual.valor;
+        }
+    }
+
+    public T maximo(){
+        if(cardinal == 0){
+            return null;
+        }else {
+            Nodo actual = this.raiz;
+            while (actual.der != null) {
+                actual = actual.der;
+            }
+            return actual.valor;
+        }
+    }
+
+    public void insertar(T elem){
+        if(!pertenece(elem)){
+            this.raiz =  insertarRec(this.raiz, null, elem);
+            cardinal ++;
+        }
+    }
+    private Nodo insertarRec(Nodo nodo, Nodo padre, T elem){
+        if(nodo == null){
+            Nodo nuevo = new Nodo(elem);
+            nuevo.padre = padre;
+            return nuevo;
+        }
+        if (nodo.valor.compareTo(elem) > 0) {
+            nodo.izq = insertarRec(nodo.izq, nodo, elem);
+        } else {
+            nodo.der = insertarRec(nodo.der, nodo, elem);
+        }
+        return nodo;
+    }
+
+    public boolean pertenece(T elem){
+        return perteneceRec(this.raiz, elem);
+    }
+
+    private boolean perteneceRec(Nodo nodo, T elem){
+        if(nodo == null){
+            return false;
+        }
+        if (nodo.valor.compareTo(elem) == 0) {
+            return true;
+        }else if (nodo.valor.compareTo(elem) > 0) {
+            return perteneceRec(nodo.izq, elem);
+        }else {
+            return perteneceRec(nodo.der, elem);
+        }
+    }
+
+    public void eliminar(T elem){
+        if(pertenece(elem)){
+            this.raiz = eliminarRec(this.raiz, elem);
+            cardinal --;
+        }
+    }
+
+    private Nodo eliminarRec(Nodo nodo, T elem){
+        if(nodo == null){
+            return null;
+        }
+        if(nodo.valor.compareTo(elem) > 0){
+            nodo.izq = eliminarRec(nodo.izq, elem);
+        }else if(nodo.valor.compareTo(elem) < 0){
+            nodo.der = eliminarRec(nodo.der, elem);
+        }else{
+            if (nodo.izq == null && nodo.der == null){ //caso sin hijos
+                return null;
+            }else if(nodo.izq == null){ //caso1 un solo hijo
+                nodo.der.padre = nodo.padre;
+                return nodo.der;
+            }else if(nodo.der == null){ //caso2 un solo hijo
+                nodo.izq.padre = nodo.padre;
+                return nodo.izq;
+            }else{ //caso dos hijos
+                Nodo sucesor = encontrarMin(nodo.der);
+                nodo.valor = sucesor.valor; 
+                nodo.der = eliminarRec(nodo.der, sucesor.valor);
+            }
+        }
+        return nodo;
+    }
+
+    private Nodo encontrarMin(Nodo nodo) {
+        while (nodo.izq != null) {
+            nodo = nodo.izq;
+        }
+        return nodo;
+    }
+
+    public String toString(){
+        throw new UnsupportedOperationException("No implementada aun");
+    }
+
+    private class ABB_Iterador implements Iterador<T> {
+        private Nodo _actual;
+
+        public boolean haySiguiente() {            
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    
+        public T siguiente() {
+            throw new UnsupportedOperationException("No implementada aun");
+        }
+    }
+
+    public Iterador<T> iterador() {
+        return new ABB_Iterador();
+    }
+
+}
